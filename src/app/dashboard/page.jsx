03ed0de723457f4ee6747ec1b777febed9192da6
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-11-06 16:38:41 +0700

    first commit and setup project

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Button>tessss</Button>
+    </div>
+  )
+}
+
+export default Dashboard
