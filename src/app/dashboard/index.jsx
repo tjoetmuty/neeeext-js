45eb107fe01ba175e704d25a998c11dfd2b1808a
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-11-06 20:54:12 +0700

    create mockup design for dashboard

--- src/app/dashboard/index.jsx
+++ src/app/dashboard/index.jsx
@@ -9,8 +9,10 @@ const Dashboard = () => {
       <Header/>
       <div className="flex">
         <SideBar/>
-        <div className="overflow-auto">
-           home
+        <div className="overflow-auto bg-gray-300 w-full h-screen">
+        {[...Array(100)].map((_, i) => (
+          <p key={i}>Home Content Line {i + 2}</p>
+        ))}
         </div>
       </div>
     </div>
